8e5026f9ffd95c526e25287bcd8f2063d8cbc57f
jbts <rhh0@protonmail.com>
2021-05-25 09:53:17 -0500

    [#20] Make Plane no longer extend/implement Primitive

--- code/geom/Plane.h
+++ code/geom/Plane.h
@@ -3,13 +3,12 @@
 
 #include "core/Point3.h"
 #include "core/Vector3.h"
-#include "geom/Primitive.h"
 #include "geom/HitInfo.h"
 #include "geom/Ray.h"
 #include "light/Material.h"
 
 /// A plane in 3D, defined by a normal and a point lying on the plane
-class Plane : public Primitive {
+class Plane {
   private:
     // A point on the plane
     Point3 p_;
@@ -23,7 +22,7 @@ class Plane : public Primitive {
     Plane() : p_(0, 0, 0), normal_(0, 1, 0), m_() {}
     Plane(const Point3& p, const Vector3& normal, const Material* const m);
 
-    HitInfo FindIntersection(const Ray& ray) const override;
+    HitInfo FindIntersection(const Ray& ray) const;
 
     /// Check whether the plane contains the given point
     bool Contains(const Point3& p) const;
